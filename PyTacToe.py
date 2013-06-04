c88f0caa666fbac3db1fcc3da48419074ee6e485
Greg Patton <greg.patton@hp.com>
2013-06-03 19:26:26 -0500

    Update PyTacToe.py
    Added Play-again loop, scoreboard, and cleaned-up lots of stuff

--- PyTacToe.py
+++ PyTacToe.py
@@ -3,15 +3,15 @@
 import random
 
 def displayInstructions():
- print '\n\n'
- print 'Enter your move using the following board positions.'
- print '\n\n'
- print ' 1 | 2 | 3'
- print '-----------'
- print ' 4 | 5 | 6'
- print '-----------'
- print ' 7 | 8 | 9'
- print '\n\n'
+  print '\n\n--------'
+  print 'PyTacToe'
+  print '--------\n\n'
+  print 'Enter your move using the following board positions.\n\n'
+  print ' 1 | 2 | 3'
+  print '-----------'
+  print ' 4 | 5 | 6'
+  print '-----------'
+  print ' 7 | 8 | 9\n\n'
 
 def displayBoard(board):
   print '\n\n'
@@ -50,61 +50,88 @@ def isWinner(board, letter):
 def commitPlay(board, letter, move):
     board[move] = letter
 
-def getCompuaterRandomInt():
+def getComputerRandomInt():
   return (random.randrange(1, 10, 1)) - 1
 
 def getUserInt():
-  print "Enter a move (1-9): "
+  print 'Enter a move (1-9): '
   move = raw_input()
   while not (move == '1' or move == '2' or move == '3' or move == '4' or move == '5' or move == '6' or move == '7' or move == '8' or move == '9'):
     print 'Invalid move\nEnter a move (1-9)'
     move = raw_input()
   return int(move) - 1
 
-def gameOver(message):
+def firstMove():
+  if random.randint(0, 1) == 0:
+    print '\nThe Computer Goes First'
+    return 'Computer'
+  else:
+    print '\nYou Go First'
+    return 'Human'
+
+def scoreboard(comp, hum, ties, message):
   print '\n\n----------------------'
   print message
-  print '----------------------\n\n'
+  print '----------------------'
+  print ' Computer:  ' + str(comp) + ' Win(s)'
+  print ' Human:     ' + str(hum) + ' Win(s)'
+  print ' Tie Games: ' + str(ties)
+  print '----------------------'
   return
 
 def main():
-  print('PyTacToe')
-  grid_list = [' ', ' ', ' ' ,' ' ,' ' ,' ' ,' ' ,' ', ' ']
   displayInstructions()
-  endOfGame = False
-  turn = 'Human'
+  play=True
+  humanWins = 0
+  computerWins = 0
+  tieGames = 0
+  while (play==True):
 
-  while (endOfGame == False):
-    while (turn == 'Human' and endOfGame == False):
-      displayBoard(grid_list)
-      move = getUserInt()
-      if (isValidMove(grid_list, move) == 0):
-        commitPlay(grid_list, 'X', move)
-        turn = 'Computer'
-      else:
-        print "That position has already been played."
-      if (isWinner(grid_list, 'X') == True):
-        gameOver("You Win!")
-        endOfGame = True
-      else:
-        if (isBoardFull(grid_list) == True):
-          gameOver("Tie Game.")
+    grid_list = [' ', ' ', ' ' ,' ' ,' ' ,' ' ,' ' ,' ', ' ']
+    endOfGame = False
+    turn = firstMove()
+
+    while (endOfGame == False):
+      while (turn == 'Human' and endOfGame == False):
+        displayBoard(grid_list)
+        move = getUserInt()
+        if (isValidMove(grid_list, move) == 0):
+          commitPlay(grid_list, 'X', move)
+          turn = 'Computer'
+        else:
+          print 'That position has already been played.'
+        if (isWinner(grid_list, 'X') == True):
+          humanWins = humanWins + 1
+          scoreboard(computerWins, humanWins, tieGames, 'You Win!!!!')
           endOfGame = True
+        else:
+          if (isBoardFull(grid_list) == True):
+            tieGames = tieGames + 1
+            scoreboard(computerWins, humanWins, tieGames, 'Tie Game.')
+            endOfGame = True
 
-    while (turn == 'Computer' and endOfGame == False):
-      move = getCompuaterRandomInt()
-      if (isValidMove(grid_list, move) == 0):
-        commitPlay(grid_list, 'O', move)
-        turn = 'Human'
-      if (isWinner(grid_list, 'O') == True):
-        gameOver("The Computer Won.")
-        endOfGame = True
-      else:
-        if (isBoardFull(grid_list) == True):
-          print "Tie Game."
+      while (turn == 'Computer' and endOfGame == False):
+        move = getComputerRandomInt()
+        if (isValidMove(grid_list, move) == 0):
+          commitPlay(grid_list, 'O', move)
+          turn = 'Human'
+        if (isWinner(grid_list, 'O') == True):
+          computerWins = computerWins + 1
+          scoreboard(computerWins, humanWins, tieGames, 'The Computer Won.')
           endOfGame = True
+        else:
+          if (isBoardFull(grid_list) == True):
+            tieGames = tieGames + 1
+            scoreboard(computerWins, humanWins, tieGames, 'Tie Game!')
+            endOfGame = True
 
-  displayBoard(grid_list)
+    displayBoard(grid_list)
+    print 'Play Again? (Y, N): '
+    again = raw_input()
+    if (again.lower().startswith('y')):
+      play=True
+    else:
+      play=False
 
 if __name__ == '__main__':
   main()
